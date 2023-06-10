8b14e7aea0ecc990eed17fc1fbdad97fa96f3ed4
Naail Kashif-Khan <naail28@gmail.com>
2023-06-10 13:46:29 +0000

    Updated dummy script

--- make_dummy_model.py
+++ make_dummy_model.py
@@ -1,15 +1,8 @@
 from transformers import AutoModelForSequenceClassification
-from transformers import AutoConfig
+from transformers import AutoConfig, AutoTokenizer
 import os
 
-#Check for output directory and create it if it doesn't exist
-if not os.path.exists("models/"):
-    os.mkdir("models")
-
-if not os.path.exists("models/dummy"):
-    os.mkdir("models/dummy")
-
-#Load the model
+#Generate a dummy fine-tuned ESM Model
 # model_name = "esm2_t30_150M_UR50D"
 # model_checkpoint = f"facebook/{model_name}"
 # model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=1)
@@ -20,3 +13,10 @@ model_path = "models/dummy/"
 model_name = "dummy_model"
 config = AutoConfig.from_pretrained(model_path)
 model = AutoModelForSequenceClassification.from_pretrained(model_path, config=config)
+
+test_sequence = "MTNLDYF"
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t30_150M_UR50D")
+tokenized = tokenizer(test_sequence, return_tensors="pt")["input_ids"]
+
+print(model(tokenized))
